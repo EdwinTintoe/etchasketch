ef59ccddef0460765900929fd95308a7921a2c2a
EdwinTintoe <davidpola159@gmail.com>
2024-11-29 22:03:31 +0000

    modify function to create grid via button & prompt

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const container = document.querySelector("#container");
+const btn = document.querySelector("#create");
 
-
-function makeSquares(rowCount){
-    const squareCount = rowCount * rowCount;
-    const squareWidth = ((1 / rowCount) * 99.9);
-    for (let i = 0; i < squareCount; i++){
-        let square = document.createElement("div");
-        square.className = "square";
-        square.style.flexBasis = `calc(${squareWidth}% - 2px)`;
-        container.appendChild(square);
+btn.addEventListener("click", ()=>{
+    container.innerHTML = "";
+    let sideSize = parseInt(prompt("Please enter the size of grid side (1-100)", "1"));
+    while (Number.isNaN(sideSize)) {
+        sideSize = parseInt(prompt("Enter a number between 1 and 100", "1"));
     }
-}
-makeSquares(20);
+    
+    const squareCount = sideSize * sideSize;
+    const squareWidth = ((1 / sideSize) * 99.9);
+        for (let i = 0; i < squareCount; i++){
+            let square = document.createElement("div");
+            square.className = "square";
+            square.style.flexBasis = `calc(${squareWidth}% - 2px)`;
+            container.appendChild(square);
+        }
+    
+});
