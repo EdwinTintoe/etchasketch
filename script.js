f66af0df9364286827ff63258fab4cc318b2c895
EdwinTintoe <davidpola159@gmail.com>
2024-11-30 11:55:19 +0000

    correct formula for square width & container style

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const container = document.querySelector("#container");
 const btn = document.querySelector("#create");
-
+function getSideSize (){
+    let size = parseInt(prompt("Please enter the size of grid side (1-100)", "1"));
+    while (Number.isNaN(size)) {
+        size = parseInt(prompt("Enter a number between 1 and 100", "1"));
+    }
+    return size;
+}
 btn.addEventListener("click", ()=>{
     container.innerHTML = "";
-    let sideSize = parseInt(prompt("Please enter the size of grid side (1-100)", "1"));
-    while (Number.isNaN(sideSize)) {
-        sideSize = parseInt(prompt("Enter a number between 1 and 100", "1"));
-    }
+    let sideSize = getSideSize();
     
     const squareCount = sideSize * sideSize;
-    const squareWidth = ((1 / sideSize) * 99.9);
+    const squareWidth = (100 / sideSize);
         for (let i = 0; i < squareCount; i++){
             let square = document.createElement("div");
             square.className = "square";
             square.style.flexBasis = `calc(${squareWidth}% - 2px)`;
+            square.textContent = `${i}`;
             container.appendChild(square);
         }
     
